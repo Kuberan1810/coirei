704c1f842b5e6f2b9efda7875f1dfe76baf6b00a
synan-mannan <syedmark786@gmail.com>
2025-12-30 19:37:04 +0530

    added hero sectiona and about section into services page

--- src/pages/services/section/ServiceAbout.tsx
+++ src/pages/services/section/ServiceAbout.tsx
@@ -1,8 +1,57 @@
-import React from 'react'
+
+import  signature  from "../../../assets/images/services/signature2.png"
+import groupImage from "../../../assets/images/services/group_images.png"
 
 const ServiceAbout = () => {
   return (
-    <div>ServiceAbout</div>
+    <section className='bg-[#1C1B1B] h-[600px] flex items-center'>
+      <div className="flex pl-20">
+          
+          {/* Image Collage */}
+          <div className="relative w-[350px] h-[400px] mr-20">
+            {/* Orange Shape */}
+            <img src={groupImage} alt="" className='object-cover w-[350px] h-[400px]'/>
+          </div>
+
+          {/* Text Content */}
+          <div className='h-[416px]'>
+            <span className="text-2xl text-white">About Us</span>
+
+            <h2 className="mt-4 text-6xl leading-tight text-white">
+              Transforming{" "}
+              <span className="text-[#FF6A00]">Ideas.</span>
+              <br />
+              
+              <p className="text-[#FF6A00]">
+                into Digital Reality
+              </p>
+            </h2>
+
+            <p className="mt-4 text-[#FFFAFA] text-sm w-[290px]">
+              Get assistance with invoices, subscriptions, plan
+              changes, or account-related questions
+            </p>
+
+            {/* Signature */}
+            <div className="mt-6">
+              <img
+                src={signature}
+                alt="Signature"
+                className="absolute text-white h-[130px] w-[130px] object-cover"
+              />
+            </div>
+            <div className="relative mt-20">
+              <p className=" text-white text-md mt-2">
+                Naveenkumar S
+                <br />
+                <span className="text-white text-sm">
+                  (CEO & Founder)
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+    </section>
   )
 }
 
--- src/pages/services/section/ServicesHero.tsx
+++ src/pages/services/section/ServicesHero.tsx
@@ -1,16 +1,50 @@
-import React from 'react'
+import bg_img_1 from "../../../assets/images/services/hero_img.jpg";
+import bg_img_2 from "../../../assets/images/services/hero_img_2.png"
 
 const ServicesHero = () => {
-    return (
-        <>
-            <section className="md:mb-80 mb-25 mt-50 ">
+  return (
+    <>
+      <section className="mb-40 mt-50 ">
+        <div className="relative w-full max-w-[1280px] mx-auto mt-8 rounded-sm overflow-hidden">
+          {/* Background Image */}
+          <img
+            src={bg_img_1}
+            alt="Hero Background"
+            className="absolute inset-0 w-full h-full object-cover  object-bottom"
+          />
 
+          {/* Right Code Overlay */}
+          <div className="absolute inset-y-0 right-0 w-[40%]  flex items-center justify-end">
+            
+              <img src={bg_img_2} alt="" className="relative object-contain"/>
+            
+          </div>
 
-                
-            </section>
+          {/* Dark Overlay */}
+          <div className="absolute inset-0 bg-black/40" />
 
-        </>
-    )
-}
+          {/* Content */}
+          <div className="relative z-10 h-[410px] flex items-center px-14">
+            <div className="max-w-xl">
+              <h1 className="text-white text-4xl leading-tight">
+                Where Creativity Meets <br />
+                Cutting - Edge Technology
+              </h1>
 
-export default ServicesHero
+              <p className="mt-7 text-[#E3E2DF] text-sm leading-relaxed w-[350px]">
+                Talk to our team about advanced capabilities, team collaboration
+                features, and enterprise-scale execution support.
+              </p>
+
+              <button className="mt-7 px-6 py-2 rounded-full border border-white/30 text-white text-xl  hover:bg-white hover:text-black transition">
+                Explore more
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default ServicesHero;
